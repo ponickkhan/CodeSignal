ffc9d49564bf54871e4680e192692eafaf1406e6
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-11-09 09:25:55 +0600

    update twostring merge lexicographic smallest string

--- GCA/mergeStrings.php
+++ GCA/mergeStrings.php
@@ -3,19 +3,51 @@ function mergeStrings($s1, $s2) {
 
     $str_arr1 = str_split($s1);
     $str_arr2 = str_split($s2);
+    $i=0;
+    $j=0;
+    $k=0;
+    $output=[];
+    for(;$i<count($str_arr1) && $j<count($str_arr2);){
 
-    for($i=0;$i<count($str_arr1);$i++){
+
+        if($str_arr1[$i] <= $str_arr2[$j]){
+
+            array_push($output,$str_arr1[$i]);
+            ++$i;
+        }else{
+            array_push($output,$str_arr2[$j]);
+            ++$j;
 
 
-        if($str_arr1[$i] > $str_arr2[$i]){
-
-            print $str_arr1[$i]." - ".$str_arr2[$i]." | ";
         }
+        ++$k;
 
     }
 
-}
-$s1="dce";
-$s2="cccbd";
+    while($i<count($str_arr1)){
+        array_push($output,$str_arr1[$i]);
+        ++$i;
+        ++$k;
 
-mergeStrings($s1, $s2);
+    }
+
+    while($j<count($str_arr2)){
+        array_push($output,$str_arr2[$j]);
+        ++$j;
+        ++$k;
+
+    }
+
+    return implode($output);
+
+
+
+}
+$s1= "dce";
+$s2= "cccbd";
+//$s1="uchlmfelno";
+//$s2="gr";
+//$s1= "ougtaleegvrabhugzyx";
+//$s2= "wvieaqgaegbxg";
+
+echo mergeStrings($s1, $s2);
